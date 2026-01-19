5981d29b679d5d81eb47c93c68e4d6ebe8ae51e7
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-01-19 12:10:31 +1100

    Added timing for the log-normalization.
    As this is what we actually care about.

--- README.md
+++ README.md
@@ -2,17 +2,11 @@
 
 ## Overview
 
-On many CPUs, floating-point division is much slower than the equivalent multiplication,
-see [discussion here](https://stackoverflow.com/questions/4125033/floating-point-division-vs-floating-point-multiplication?noredirect=1&lq=1).
+On many CPUs, floating-point division is much slower than multiplication,
+see [discussion here](https://stackoverflow.com/questions/4125033/floating-point-division-vs-floating-point-multiplication).
 Thus, when dividing by the same denominator in a loop, one might consider replacing `x / y` with a `x * z` where `z = 1 / y` is computed outside of the loop.
 This sacrifices some accuracy (due to the round-off error from computing `z`) in order to replace the per-iteration divide with a multiply.
 The question is, does this actually have an impact?
-
-## Setup
-
-We test the speed of iterating over an array and dividing its contents by a constant.
-This is a pretty common pattern in **tatami**, and while usually more operations are involved, we'll keep things simple here.
-We compare to the alternative, i.e., compute the reciprocal and use it to multiply each array element within the loop.
 
 To build, use the usual CMake process:
 
@@ -21,16 +15,38 @@ cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
 cmake --build build
 ```
 
-## Results
+## Basic test
+
+We test the speed of iterating over an array and dividing its contents by a constant.
+This is a pretty common pattern in **tatami**, and while usually more operations are involved, we'll keep things simple here.
+We compare to the alternative, i.e., compute the reciprocal and use it to multiply each array element within the loop.
 
 On an Intel i7-8850H, we get:
 
 ```
-$ ./build/divtest
+$ ./build/basic
 Division:      	0.0137299 ± 1.95084e-05
 Multiplication:	0.0136978 ± 3.16613e-05
 ```
 
 Not much effect, it seems.
-A few runs indicate that the <1% improvement is consistent, so the multiplication is faster but not by much.
 I'd guess that the CPU is bottlenecked by reading the array from main memory, such that the speed of the actual operation doesn't matter.
+
+## Log-normalization
+
+A more robust test involves log-normalization of a count matrix in RNA-seq data.
+This involves computing $\log_2(x / f + 1)$ where $x$ is the array of data and $f$ is a constant size factor.
+We consider several permutations of this approach using `log1p` instead of `log2` and/or multiplying by the reciprocal.
+
+On an Intel i7-8850H, we get:
+
+```
+$ ./build/normalize
+log1p(x / f) / log(2): 0.144089 ± 0.000512589
+log1p(x * r) / log(2): 0.12879 ± 0.000733333
+log2(x / f + 1):       0.0981286 ± 0.000795611
+log2(x * r + 1):       0.0792897 ± 0.000827526
+```
+
+Division is significantly faster here.
+Now that we're doing a decent amount of work with the log-transformations, compute is now the bottleneck and the extra time spent in the divide is no longer masked.
---
+++ normalize.cpp
@@ -0,0 +1,96 @@
+#include "eztimer/eztimer.hpp"
+
+#include "CLI/App.hpp"
+#include "CLI/Formatter.hpp"
+#include "CLI/Config.hpp"
+
+#include <random>
+#include <vector>
+#include <optional>
+#include <iostream>
+
+int main(int argc, char ** argv) {
+    CLI::App app{"Log-normalization performance tests"};
+    std::size_t len;
+    app.add_option("-l,--length", len, "Length of the simulated vector")->default_val(10000000);
+    int iterations;
+    app.add_option("-i,--iter", iterations, "Number of iterations")->default_val(10);
+    unsigned long long seed;
+    app.add_option("-s,--seed", seed, "Seed for the simulated data")->default_val(69);
+    CLI11_PARSE(app, argc, argv);
+
+    // Setting up the simulated data.
+    std::vector<double> inputs(len);
+    std::mt19937_64 rng(seed);
+    std::uniform_real_distribution<> unif;
+    for (auto& i : inputs) {
+        i = unif(rng);
+    }
+
+    const double denom = unif(rng);
+    const double denom2 = 1.0/denom;
+
+    // Setting up the functions.
+    std::vector<std::function<double()> > funs;
+    funs.reserve(2);
+
+    std::vector<double> log1pdiv_results(len);
+    funs.emplace_back([&]() -> double {
+        for (std::size_t i = 0; i < len; ++i) {
+            log1pdiv_results[i] = std::log1p(inputs[i] / denom) / std::log(2);
+        }
+        return log1pdiv_results.front() + log1pdiv_results[len/2] + log1pdiv_results.back();
+    });
+
+    std::vector<double> log1pmult_results(len);
+    funs.emplace_back([&]() -> double {
+        for (std::size_t i = 0; i < len; ++i) {
+            log1pmult_results[i] = std::log1p(inputs[i] * denom2) / std::log(2);
+        }
+        return log1pmult_results.front() + log1pmult_results[len/2] + log1pmult_results.back();
+    });
+
+    std::vector<double> log2div_results(len);
+    funs.emplace_back([&]() -> double {
+        for (std::size_t i = 0; i < len; ++i) {
+            log2div_results[i] = std::log2(inputs[i] / denom + 1); 
+        }
+        return log2div_results.front() + log2div_results[len/2] + log2div_results.back();
+    });
+
+    std::vector<double> log2mult_results(len);
+    funs.emplace_back([&]() -> double {
+        for (std::size_t i = 0; i < len; ++i) {
+            log2mult_results[i] = std::log2(inputs[i] * denom2 + 1); 
+        }
+        return log2mult_results.front() + log2mult_results[len/2] + log2mult_results.back();
+    });
+
+    // Performing the iterations.
+    eztimer::Options opt;
+    opt.iterations = iterations;
+
+    std::vector<std::optional<double> > results(funs.size());
+    auto res = eztimer::time<double>(
+        funs,
+        [&](const double& res, std::size_t i) -> void {
+            if (results[i].has_value()) {
+                if (*(results[i]) != res) {
+                    throw std::runtime_error("oops that's not right");
+                }
+            } else {
+                results[i] = res;
+                std::cout << i << " : " << res << std::endl;
+            }
+        },
+        opt
+    );
+
+    std::cout << "log1p(x / f) / log(2): " << res[0].mean.count() << " ± " << res[0].sd.count() / std::sqrt(res[0].times.size()) << std::endl;
+    std::cout << "log1p(x * r) / log(2): " << res[1].mean.count() << " ± " << res[1].sd.count() / std::sqrt(res[1].times.size()) << std::endl;
+    std::cout << "log2(x / f + 1):       " << res[2].mean.count() << " ± " << res[2].sd.count() / std::sqrt(res[2].times.size()) << std::endl;
+    std::cout << "log2(x * r + 1):       " << res[3].mean.count() << " ± " << res[3].sd.count() / std::sqrt(res[3].times.size()) << std::endl;
+
+    return 0;
+}
+
